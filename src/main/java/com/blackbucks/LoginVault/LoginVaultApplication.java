bf2e8ea07258a5644138cb76ebae29e05e994649
Smit Thakur <smitthakur@gmail.com>
2024-07-17 16:24:32 +0530

    Initial commit

---
+++ src/main/java/com/blackbucks/LoginVault/LoginVaultApplication.java
@@ -0,0 +1,13 @@
+package com.blackbucks.LoginVault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blackbucks/LoginVault/LoginVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackbucks.LoginVault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
